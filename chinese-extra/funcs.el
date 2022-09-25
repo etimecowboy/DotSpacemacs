f342f077a9c9cb7652bed8fdb96e042815aed763
Xin <etimecowboy@gmail.com>
2022-09-25 23:52:53 +0800

    change font settings.

--- chinese-extra/funcs.el
+++ chinese-extra/funcs.el
@@ -1,5 +1,5 @@
 ;;; funcs.el --- Chinese-extra Layer functions File for Spacemacs
-;; Time-stamp: <2022-09-24 Sat 07:59 by xin on tufg>
+;; Time-stamp: <2022-09-25 Sun 02:41 by xin on tufg>
 ;; Author: etimecowboy <etimecowboy@gmail.com>
 ;;
 ;; This file is not part of GNU Emacs.
@@ -22,24 +22,66 @@
     nil))
 
 (defun xy/set-fonts ()
-  "Setup different fonts for default, Chinese, symbols, and emoji."
+  "Setup different fonts for default, Chinese, and emoji."
   (interactive)
   (when (display-graphic-p)
+
     ;; Set default font
-    (cl-loop for font in '("Cascadia Code"
-                           "Fira Code"
-                           "Iosevka"
-                           "Source Code Pro"
-                           "Consolas"
-                           "Jetbrains Mono"
-                           "Hack"
-                           "Menlo"
-                           "DejaVu Sans Mono"
-                           )
+    (cl-loop for font in
+             '("Cascadia Code"
+               "Fira Code"
+               "Iosevka"
+               "Source Code Pro"
+               "Consolas"
+               "Jetbrains Mono"
+               "Hack"
+               "Menlo"
+               "DejaVu Sans Mono"
+               )
              when (font-installed-p font)
-             return (set-face-attribute 'default nil
-                                        :family font
-                                        :height 110))
+             return (set-face-attribute 'default nil :family font :height 110))
+
+    ;; Set font for Chinese characters
+    (cl-loop for font in
+             '("Adobe Fangsong Std"
+               "WenQuanYi Micro Hei Mono"
+               "FZDaHei-B02"
+               "Microsoft Yahei"
+               "Adobe Heiti Std"
+               "FZXiaoBiaoSong-B05"
+               "Adobe Song Std"
+               "Adobe Kaiti Std"
+               "LXGW WenKai Mono GB"
+               )
+             when (font-installed-p font)
+             return (progn
+                      (setq face-font-rescale-alist `((,font . 1.25)))
+                      (set-fontset-font t 'han (font-spec :family font))))
+
+    ;; Set font for color emojis
+    (cl-loop for font in
+             '("Twitter Color Emoji"
+               "NotoEmoji Nerd Font Mono"
+               "Noto Color Emoji"
+               "Apple Color Emoji")
+             when (font-installed-p font)
+             return (if (>= emacs-major-version 28)
+                        (set-fontset-font t 'emoji (font-spec :family font) nil 'prepend)
+                      (set-fontset-font t 'symbol (font-spec :family font) nil 'prepend)))
+
+    ;; FIXME: no working
+    ;; Set mode-line font
+    ;; (cl-loop for font in
+    ;;          '("Menlo"
+    ;;            "SF Pro Display"
+    ;;            "Helvetica")
+    ;;          when (font-installed-p font)
+    ;;          return (progn
+    ;;                   (set-face-attribute 'mode-line nil :family font :height 90)
+    ;;                   (when (facep 'mode-line-active)
+    ;;                     (set-face-attribute 'mode-line-active nil :family font :height 90))
+    ;;                   (set-face-attribute 'mode-line-inactive nil :family font :height 90)))
+
 
     ;; FIXME: This cause wrong display of all-the-icons (in treemacs, dired, minibuffer, and etc.)
     ;; Specify font for all unicode characters
@@ -47,31 +89,4 @@
     ;;          when (font-installed-p font)
     ;;          return (set-fontset-font t 'unicode font nil 'prepend))
 
-    ;; Emoji
-    (cl-loop for font in '("Twitter Color Emoji"
-                           "NotoEmoji Nerd Font Mono"
-                           "Noto Color Emoji"
-                           "Apple Color Emoji")
-             when (font-installed-p font)
-             return (if (>= emacs-major-version 28)
-                        (set-fontset-font t 'emoji (font-spec :family font) nil 'prepend)
-                      (set-fontset-font t 'symbol (font-spec :family font) nil 'prepend)))
-
-    ;; Specify font for Chinese characters
-    (cl-loop for font in '("FZXiaoBiaoSong-B05" ;; serif font
-                           "FZDaHei-B02" ;; sans serif font
-                           "Microsoft Yahei"
-                           "Adobe Heiti Std"
-                           "WenQuanYi Micro Hei Mono"
-                           "LXGW WenKai Mono GB"
-                           ;; "FZXiaoBiaoSong-B05"
-                           "Adobe Fangsong Std"
-                           "Adobe Song Std"
-                           "Adobe Kaiti Std"
-                           )
-             when (font-installed-p font)
-             return (progn
-                      ;; (setq face-font-rescale-alist `((,font . 1.3))) ;; good for wqy-microhei
-                      (setq face-font-rescale-alist `((,font . 1.2)))
-                      (set-fontset-font t 'han (font-spec :family font))))
     ))
