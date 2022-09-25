faafe042a99fc1b46f38f120fc7e888749c89225
Xin <etimecowboy@gmail.com>
2022-09-25 23:53:36 +0800

    Change emacs-demo layer keys.

--- emacs-demo/packages.el
+++ emacs-demo/packages.el
@@ -1,6 +1,6 @@
 ;; -*- mode: emacs-lisp; lexical-binding: t -*-
 ;;; packages.el --- emacs-demo layer packages file for Spacemacs.
-;; Time-stamp: <2022-09-24 Sat 09:39 by xin on tufg>
+;; Time-stamp: <2022-09-25 Sun 01:32 by xin on tufg>
 ;; Author: etimecowboy <etimecowboy@gmail.com>
 ;;
 ;; This file is not part of GNU Emacs.
@@ -32,7 +32,8 @@
 (defun emacs-demo/init-gif-screencast ()
   (use-package gif-screencast
     :init
-    (spacemacs/set-leader-keys "Cg" 'gif-screencast)
+    (spacemacs/declare-prefix "aD" "demo")
+    (spacemacs/set-leader-keys "aDg" 'gif-screencast)
     :bind
     (:map gif-screencast-mode-map
           ("<f8>" . gif-screencast-toggle-pause)
@@ -43,4 +44,7 @@
   (use-package keycast
     :init
     (spacemacs/declare-prefix "aD" "demo")
-    (spacemacs/set-leader-keys "aDk" 'keycast-tab-bar-mode)))
+    (spacemacs/set-leader-keys "aDt" 'keycast-tab-bar-mode)
+    :config
+    (setq keycast-tab-bar-location  'replace
+          keycast-tab-bar-format "%k%2s%c%R")))
