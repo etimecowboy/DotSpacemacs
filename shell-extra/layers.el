3ca4c0ceb95a1d64ef31ac47eb2c454b806dbf9b
Xin <etimecowboy@gmail.com>
2022-11-23 17:31:53 +0800

    Add layer dependency for shell-extra layer.

---
+++ shell-extra/layers.el
@@ -0,0 +1,13 @@
+;;; layers.el --- shell-extra layers File for Spacemacs
+;; Time-stamp: <2022-11-23 Wed 07:54 by xin on tufg>
+;; Author: etimecowboy <etimecowboy@gmail.com>
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+;;
+;;; Commentary:
+;;
+;;; Code:
+
+(configuration-layer/declare-layer-dependencies '(shell))
