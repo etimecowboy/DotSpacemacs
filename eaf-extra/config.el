bb27e0aa6970d867af2b9d7d46228ebb3830ba4a
Xin <etimecowboy@gmail.com>
2022-11-23 17:28:02 +0800

    Move cofig from .spacemacs to eaf-extra layer.

---
+++ eaf-extra/config.el
@@ -0,0 +1,16 @@
+;;; config.el --- eaf-extra configuration File for Spacemacs
+;; Time-stamp: <2022-11-23 Wed 07:08 by xin on tufg>
+;; Author: etimecowboy <etimecowboy@gmail.com>
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+;;
+;;; Commentary:
+;;
+
+;;; Code:
+(with-eval-after-load "eaf"
+  (eaf-setq eaf-browser-enable-adblocker "true"))
+
+(xy/set-google-chrome-as-default)
---
+++ eaf-extra/keybindings.el
@@ -0,0 +1,14 @@
+;; -*- mode: emacs-lisp; lexical-binding: t -*-
+;;; keybindings.el --- eaf-extra Layer keybindings File for Spacemacs
+;; Time-stamp: <2022-11-23 Wed 06:52 by xin on tufg>
+;; Author: etimecowboy <etimecowboy@gmail.com>
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+;;
+;;; Commentary:
+;;
+;;; Code:
+
+(spacemacs/set-leader-keys "te" 'xy/toggle-eaf-browser)
