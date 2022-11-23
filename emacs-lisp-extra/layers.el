3edb05af807967f776e9d10af4ad793f4d7af60b
Xin <etimecowboy@gmail.com>
2022-11-23 17:28:33 +0800

    Add layer dependency in emacs-lisp-extra layer.

---
+++ emacs-lisp-extra/layers.el
@@ -0,0 +1,14 @@
+;;; layers.el --- emacs-lisp-extra layers File for Spacemacs
+;; Time-stamp: <2022-11-23 Wed 07:57 by xin on tufg>
+;; Author: etimecowboy <etimecowboy@gmail.com>
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+;;
+;;; Commentary:
+;;
+;;; Code:
+
+(configuration-layer/declare-layer-dependencies '(emacs-lisp))
+
