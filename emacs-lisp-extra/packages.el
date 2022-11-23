4e7b92c857c797b941240e1c14dea808a1c24895
Xin <etimecowboy@gmail.com>
2022-11-23 17:37:20 +0800

    Minor changes.

--- emacs-lisp-extra/packages.el
+++ emacs-lisp-extra/packages.el
@@ -1,6 +1,6 @@
 ;; -*- mode: emacs-lisp; lexical-binding: t -*-
 ;;; packages.el --- emacs-lisp-extra layer packages file for Spacemacs.
-;; Time-stamp: <2022-09-17 Sat 02:31 by xin on tufg>
+;; Time-stamp: <2022-11-23 Wed 07:57 by xin on tufg>
 ;; Author: etimecowboy <etimecowboy@gmail.com>
 ;;
 ;; This file is not part of GNU Emacs.
@@ -18,5 +18,3 @@
 
 (defun emacs-lisp-extra/init-cask ()
   (use-package cask))
-
-
