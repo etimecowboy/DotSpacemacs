779286fb6697d990be65247689df2dd5ea2fb515
Xin <etimecowboy@gmail.com>
2022-12-10 15:40:11 +0800

    Enable proxy for eaf-browser.

--- eaf-extra/config.el
+++ eaf-extra/config.el
@@ -1,5 +1,5 @@
 ;;; config.el --- eaf-extra configuration File for Spacemacs
-;; Time-stamp: <2022-11-24 Thu 16:44 by xin on tufg>
+;; Time-stamp: <2022-12-03 Sat 02:17 by xin on tufg>
 ;; Author: etimecowboy <etimecowboy@gmail.com>
 ;;
 ;; This file is not part of GNU Emacs.
@@ -11,4 +11,7 @@
 
 ;;; Code:
 (with-eval-after-load "eaf"
-  (eaf-setq eaf-browser-enable-adblocker "true"))
+  (eaf-setq eaf-browser-enable-adblocker "true")
+  (setq eaf-proxy-type "http"
+        eaf-proxy-host "127.0.0.1"
+        eaf-proxy-port "7890"))
