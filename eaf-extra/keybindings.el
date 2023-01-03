34d3a4edd15540edc71315d8301bf20fce693eb3
Xin <etimecowboy@gmail.com>
2023-01-03 22:56:24 +0800

    Add more eaf apps.

--- eaf-extra/keybindings.el
+++ eaf-extra/keybindings.el
@@ -1,6 +1,6 @@
 ;; -*- mode: emacs-lisp; lexical-binding: t -*-
 ;;; keybindings.el --- eaf-extra Layer keybindings File for Spacemacs
-;; Time-stamp: <2022-11-23 Wed 06:52 by xin on tufg>
+;; Time-stamp: <2023-01-03 Tue 14:54 by xin on tufg>
 ;; Author: etimecowboy <etimecowboy@gmail.com>
 ;;
 ;; This file is not part of GNU Emacs.
@@ -12,3 +12,7 @@
 ;;; Code:
 
 (spacemacs/set-leader-keys "te" 'xy/toggle-eaf-browser)
+(spacemacs/set-leader-keys "aabt" 'xy/toggle-eaf-browser)
+(spacemacs/set-leader-keys "aar" 'eaf-open-rss-reader)
+(spacemacs/set-leader-keys "aag" 'eaf-open-git)
+(spacemacs/set-leader-keys "aap" 'eaf-open-pdf-from-history)
