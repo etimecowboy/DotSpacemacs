708ddce8b223cdcdcf7379cca98952463ac99b88
Xin <etimecowboy@gmail.com>
2023-01-08 11:06:50 +0800

    Add aweshell keybindings.

---
+++ shell-extra/keybindings.el
@@ -0,0 +1,14 @@
+;; -*- mode: emacs-lisp; lexical-binding: t -*-
+;;; keybindings.el --- shell-extra Layer keybindings File for Spacemacs
+;; Time-stamp: <2023-01-05 Thu 06:40 by xin on tufg>
+;; Author: etimecowboy <etimecowboy@gmail.com>
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+;;
+;;; Commentary:
+;;
+;;; Code:
+
+(spacemacs/set-leader-keys "atsa" 'aweshell-dedicated-toggle)
