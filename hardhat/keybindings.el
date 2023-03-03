a46b16a9b58f965b1ede038f1f1c2191f9d410a0
Xin <etimecowboy@gmail.com>
2023-03-04 06:10:50 +0800

    Minor changes.

---
+++ hardhat/keybindings.el
@@ -0,0 +1,15 @@
+;; -*- mode: emacs-lisp; lexical-binding: t -*-
+;;; keybindings.el --- hardhat Layer keybindings File for Spacemacs
+;; Time-stamp: <2023-03-02 Thu 01:54 by xin on tufg>
+;; Author: etimecowboy <etimecowboy@gmail.com>
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+;;
+;;; Commentary:
+;;
+;;; Code:
+
+(spacemacs/set-leader-keys
+  "tA" 'global-hardhat-mode)
