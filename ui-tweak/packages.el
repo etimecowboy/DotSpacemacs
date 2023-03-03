3b995bbd18940ee1c51fbe292cc9d6917d417b80
Xin <etimecowboy@gmail.com>
2023-03-04 06:14:25 +0800

    ui-tweak minor changes.

--- ui-tweak/packages.el
+++ ui-tweak/packages.el
@@ -8,29 +8,41 @@
 ;;
 ;;; License: GPLv3
 
-(defconst ui-tweak-packages
-  '(mini-frame
-    ))
+;; (defconst ui-tweak-packages
+;;   '(;; mini-frame ;; not compatible with embark
+;;     ;; (mini-modeline :location (recipe :fetcher github :repo "kiennq/emacs-mini-modeline")) ;; not compatible with eyebrowse or persp-mode
+;;     ))
 
 
-(defun ui-tweak/init-mini-frame ()
-  (use-package mini-frame
-    :defer t
-    :config
-    (setq mini-frame-advice-functions
-          '(read-from-minibuffer read-string yes-or-no-p
-                                 spacemacs/helm-M-x-fuzzy-matching helm-M-x
-                                 spacemacs/helm-find-files helm-find-files
-                                 helm-buffers-list
-                                 )
-          ;; mini-frame-show-parameters
-          ;;   '((top . 0)
-          ;;     (width . 1.0)
-          ;;     (left . 0.5)
-          ;;     (height . 15))
-          mini-frame-completions-focus 'completions
-          mini-frame-resize-max-height 25
-          mini-frame-resize-min-height 5
-          mini-frame-standalone t
-          )
-    ))
+;; (defun ui-tweak/init-mini-frame ()
+;;   (use-package mini-frame
+;;     :defer t
+;;     :config
+;;     (setq ;; mini-frame-advice-functions
+;;           ;; '(read-from-minibuffer read-string yes-or-no-p
+;;           ;;                        spacemacs/helm-M-x-fuzzy-matching helm-M-x
+;;           ;;                        spacemacs/helm-find-files helm-find-files
+;;           ;;                        helm-buffers-list
+;;           ;;                        )
+;;           mini-frame-show-parameters
+;;             '((top . 10)
+;;               (width . 0.7)
+;;               (left . 0.5)
+;;               ;;(height . 15)
+;;               )
+;;           ;; mini-frame-completions-focus 'completions
+;;           ;; mini-frame-resize-max-height 25
+;;           ;; mini-frame-resize-min-height 5
+;;           ;; mini-frame-standalone t
+;;           )
+;;     ))
+
+;; (defun ui-tweak/init-mini-modeline ()
+;;   (use-package mini-modeline
+;;     ;;:after smart-mode-line
+;;     :after spaceline
+;;     :config
+;;     (setq mini-modeline-truncate-p nil)
+;;     ;;(mini-modeline-mode t)
+;;     )
+;;   )
