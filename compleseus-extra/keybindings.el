073bf035f04ac2045dda2ca30cc34eeeae01aa88
Xin <etimecowboy@gmail.com>
2023-03-29 09:26:50 +0800

    Add a global key for eli-select-images.

--- compleseus-extra/keybindings.el
+++ compleseus-extra/keybindings.el
@@ -1,6 +1,6 @@
 ;; -*- mode: emacs-lisp; lexical-binding: t -*-
 ;;; keybindings.el --- compleseus-extra Layer keybindings File for Spacemacs
-;; Time-stamp: <2022-12-13 Tue 17:39 by xin on tufg>
+;; Time-stamp: <2023-03-29 Wed 01:18 by xin on tufg>
 ;; Author: etimecowboy <etimecowboy@gmail.com>
 ;;
 ;; This file is not part of GNU Emacs.
@@ -14,3 +14,4 @@
 (global-set-key (kbd "M-O") 'embark-act-noquit)
 (global-set-key (kbd "M-L") 'embark-live)
 (global-set-key (kbd "M-B") 'embark-become)
+(global-set-key (kbd "M-I") 'eli-select-images)
