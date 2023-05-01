6934cd05e1e58cd42df06eca97e61e577bbaa2c1
Xin <etimecowboy@gmail.com>
2023-05-01 22:43:21 +0800

    Disable vertico-posframe.

--- compleseus-extra/packages.el
+++ compleseus-extra/packages.el
@@ -1,6 +1,6 @@
 ;; -*- mode: emacs-lisp; lexical-binding: t -*-
 ;;; packages.el --- compleseus-extra layer packages file for Spacemacs.
-;; Time-stamp: <2023-04-14 Fri 01:57 by xin on tufg>
+;; Time-stamp: <2023-05-01 Mon 09:02 by xin on tufg>
 ;; Author: etimecowboy <etimecowboy@gmail.com>
 ;;
 ;; This file is not part of GNU Emacs.
@@ -117,10 +117,10 @@
 
 (defun compleseus-extra/init-vertico-posframe ()
   (use-package vertico-posframe
-    :ensure t
+    :commands vertico-posframe-mode
     :after (vertico posframe)
     :config
     (setq vertico-posframe-fallback-mode 'vertico-buffer-mode
           vertico-posframe-poshandler 'posframe-poshandler-point-frame-center)
-    (vertico-posframe-mode 1)
+    ;; (vertico-posframe-mode 1)
     ))
