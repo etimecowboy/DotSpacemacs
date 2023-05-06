ae161f956f222c12d690c48fd5adf9560e35c107
Xin <etimecowboy@gmail.com>
2023-05-06 11:57:52 +0800

    Move config.el to packages.el.

--- compleseus-extra/packages.el
+++ compleseus-extra/packages.el
@@ -1,6 +1,6 @@
 ;; -*- mode: emacs-lisp; lexical-binding: t -*-
 ;;; packages.el --- compleseus-extra layer packages file for Spacemacs.
-;; Time-stamp: <2023-05-06 Sat 03:31 by xin on tufg>
+;; Time-stamp: <2023-05-06 Sat 03:56 by xin on tufg>
 ;; Author: etimecowboy <etimecowboy@gmail.com>
 ;;
 ;; This file is not part of GNU Emacs.
@@ -13,6 +13,12 @@
 
 (setq compleseus-extra-packages
       '(
+        ;;---- official compleseus layer packages
+        embark
+        consult
+        marginalia
+        ;; popwin ;; speck-checking layer
+        ;;---- added packages
         consult-dir
         (eli-image :location local) ;; only works in minibuffer, not in vertico-posframe-mode
         ;; consult-project-extra ;; not as good as consult-projectile
@@ -27,6 +33,118 @@
 	      ;; vertico-repeat
 	      vertico-posframe
         ))
+
+(defun compleseus-extra/pre-init-embark ()
+  (spacemacs|use-package-add-hook embark
+    :post-config
+    (add-to-list 'display-buffer-alist
+                 '("\\*Embark" display-buffer-same-window)
+                 ;; '("Embark\\ Live" display-buffer-pop-up-frame)
+                 ;; '("Embark\\ Export" display-buffer-at-bottom)
+                 ;; '("^\\*Embark\\ Live.*\\*$" display-buffer-pop-up-frame)
+                 ;; '("^\\*Embark\\ Export.*\\*$" display-buffer-at-bottom)
+                 ;; '("^\\*Embark.*\\*$" display-buffer-at-bottom)
+                 )
+    (setq embark-quit-after-action t)
+
+    ;;REF: https://karthinks.com/software/fifteen-ways-to-use-embark/
+    (eval-when-compile
+      (defmacro xy/embark-ace-action (fn)
+        `(defun ,(intern (concat "my/embark-ace-" (symbol-name fn))) ()
+           (interactive)
+           (with-demoted-errors "%s"
+             (require 'ace-window)
+             (let* ((aw-dispatch-always t)
+                    (embark-quit-after-action t))
+               (aw-switch-to-window (aw-select nil))
+               (call-interactively (symbol-function ',fn)))))))
+    (define-key embark-file-map
+                (kbd "o") (xy/embark-ace-action find-file))
+    (define-key embark-buffer-map
+                (kbd "o") (xy/embark-ace-action switch-to-buffer))
+    (define-key embark-bookmark-map
+                (kbd "o") (xy/embark-ace-action bookmark-jump))
+    (define-key embark-org-link-map
+                (kbd "o") (xy/embark-ace-action org-open-at-point))
+
+    (eval-when-compile
+      (defmacro xy/embark-split-action (fn split-type)
+        `(defun ,(intern (concat "xy/embark-"
+                                 (symbol-name fn)
+                                 "-"
+                                 (car (last (split-string
+                                             (symbol-name split-type) "-"))))) ()
+           (interactive)
+           (funcall #',split-type)
+           (call-interactively #',fn))))
+    (define-key embark-file-map
+                (kbd "2") (xy/embark-split-action find-file split-window-below))
+    (define-key embark-buffer-map
+                (kbd "2") (xy/embark-split-action switch-to-buffer split-window-below))
+    (define-key embark-bookmark-map
+                (kbd "2") (xy/embark-split-action bookmark-jump split-window-below))
+    (define-key embark-org-link-map
+                (kbd "2") (xy/embark-split-action org-open-at-point split-window-below))
+    (define-key embark-file-map
+                (kbd "3") (xy/embark-split-action find-file split-window-right))
+    (define-key embark-buffer-map
+                (kbd "3") (xy/embark-split-action switch-to-buffer split-window-right))
+    (define-key embark-bookmark-map
+                (kbd "3") (xy/embark-split-action bookmark-jump split-window-right))
+    (define-key embark-org-link-map
+                (kbd "3") (xy/embark-split-action org-open-at-point split-window-right))
+    (define-key embark-file-map
+                (kbd "S") 'sudo-find-file)
+    (define-key embark-bookmark-map
+                (kbd "S") 'sudo-find-file)
+    ))
+
+(defun compleseus-extra/pre-init-consult ()
+  (spacemacs|use-package-add-hook consult
+    :post-config
+    (consult-customize
+     consult-theme
+     :preview-key '("M-.")
+     consult-buffer
+     consult-ripgrep
+     consult-git-grep
+     consult-grep
+     consult-bookmark
+     consult-yank-pop
+     :preview-key '("M-."))
+
+    ;; (require 'consult-xref)
+    ;; consult-xref
+    consult--source-bookmark
+    consult--source-file-register
+    consult--source-recent-file
+    consult--source-project-recent-file
+
+    ;; REF: https://github.com/minad/consult/blob/main/README.org#miscellaneous
+    ;; ;; Use `consult-completion-in-region' if Vertico is enabled.
+    ;; ;; Otherwise use the default `completion--in-region' function.
+    ;; (setq completion-in-region-function
+    ;;       (lambda (&rest args)
+    ;;         (apply (if vertico-mode
+    ;;                    #'consult-completion-in-region
+    ;;                  #'completion--in-region)
+    ;;                args)))
+
+    ;; REF: https://github.com/minad/consult/issues/350
+    ;; vertico-mode is enabled at startup and I might then disable
+    ;; it interactively to quickly try something else.
+    (setq completion-in-region-function
+          (lambda (start end collection &optional predicate)
+            (if vertico-mode
+                (consult-completion-in-region start end collection predicate)
+              (completion--in-region start end collection predicate))))
+    ))
+
+(defun compleseus-extra/pre-init-marginalia ()
+  (spacemacs|use-package-add-hook marginalia
+    :post-config
+    (setq marginalia-separator "  |  ")
+    ))
 
 (defun compleseus-extra/init-consult-dir ()
   (use-package consult-dir
