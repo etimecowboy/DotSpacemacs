6cc441e85da273b15d429ef609cfd145a0efecba
Xin <etimecowboy@gmail.com>
2023-05-17 18:43:17 +0800

    Add more apps.

--- eaf-extra/keybindings.el
+++ eaf-extra/keybindings.el
@@ -1,6 +1,6 @@
 ;; -*- mode: emacs-lisp; lexical-binding: t -*-
 ;;; keybindings.el --- eaf-extra Layer keybindings File for Spacemacs
-;; Time-stamp: <2023-04-10 Mon 10:18 by xin on tufg>
+;; Time-stamp: <2023-05-17 Wed 09:09 by xin on tufg>
 ;; Author: etimecowboy <etimecowboy@gmail.com>
 ;;
 ;; This file is not part of GNU Emacs.
@@ -18,11 +18,18 @@
   "aabO" 'eaf-open-browser-other-window
   "aabB" 'eaf-open-browser-in-background
   "aaa"  'browse-url-at-point
-  "aaA"  'browse-url-chrome
+  "aaC"  'browse-url-chrome
   "aae"  'eaf-open-url-at-point
   "aar"  'eaf-open-rss-reader
   "aap"  'eaf-open-pdf-from-history
-  ;; "aag" 'eaf-open-git
+  "aaF"  'eaf-open-file-manager
+  "aaB"  'eaf-file-browser-qrcode
+  "aaS"  'eaf-file-sender-qrcode
+  "aaa"  'eaf-open-airshare
+  "aag"  'eaf-open-git
   ;; "aai" 'eaf-open-ipython
   ;; "aax" 'eaf-open-map
   )
+
+;; (define-key dired-mode-map (kbd "C-m") 'eaf-open-in-file-manager)
+(define-key dired-mode-map (kbd "C-q") 'eaf-file-sender-qrcode-in-dired)
--- eaf-extra/packages.el
+++ eaf-extra/packages.el
@@ -1,5 +1,5 @@
 ;;; packages.el --- eaf-extra layer packages File for Spacemacs
-;; Time-stamp: <2023-05-11 Thu 08:07 by xin on tufg>
+;; Time-stamp: <2023-05-17 Wed 09:09 by xin on tufg>
 ;; Author: etimecowboy <etimecowboy@gmail.com>
 ;;
 ;; This file is not part of GNU Emacs.
@@ -18,63 +18,80 @@
   (spacemacs|use-package-add-hook eaf
     :pre-init
     (setq eaf-apps-to-install
-          '(browser
+          '(airshare
+            browser
+            camera
             pdf-viewer
-            ;; music-player
-            ;; video-player
+            music-player
+            video-player
             image-viewer
-            ;; system-monitor
             terminal
-            ;; git
-            ;; map
             rss-reader
-            ;;mindmap
-            ;;markmap
-            ;;markdown-previewer
-            ;;org-previewer
-            ;;file-manager ;;FIXME: failed to install
-            ;;file-browser
-            ))
-    (setq eaf-apps
-          '(browser
-            pdf-viewer
-            ;; music-player
-            ;; video-player
-            image-viewer
-            ;; system-monitor
-            terminal
-            ;; git
-            ;; map
-            ;; rss-reader
+            markdown-previewer
+            org-previewer
+            file-manager
+            file-browser
+            file-sender
+            git
             ;; mindmap
             ;; markmap
-            ;; markdown-previewer
-            ;; org-previewer
-            ;;file-manager ;;FIXME: failed to install
-            ;;file-browser
+            ;; netease-cloud-music
+            ;; js-video-player
+            ;; system-monitor
+            ;; map
+            ))
+    (setq eaf-apps
+          '(airshare
+            browser
+            camera
+            pdf-viewer
+            music-player
+            video-player
+            image-viewer
+            terminal
+            rss-reader
+            markdown-previewer
+            org-previewer
+            file-manager
+            file-browser
+            file-sender
+            git
+            ;; mindmap
+            ;; markmap
+            ;; netease-cloud-music
+            ;; js-video-player
+            ;; system-monitor
+            ;; map
             ))
     :post-config
+    (require 'eaf-airshare)
     (require 'eaf-browser)
-    (require 'eaf-pdf-viewer)
-    ;; (require 'eaf-music-player)
-    ;; (require 'eaf-video-player)
-    (require 'eaf-image-viewer)
-    ;; (require 'eaf-system-monitor)
     (require 'eaf-terminal)
-    ;; (require 'eaf-git)
+    (require 'eaf-camera)
+    (require 'eaf-video-player)
+    (require 'eaf-file-manager)
+    (require 'eaf-file-browser)
+    (require 'eaf-file-sender)
+    (require 'eaf-image-viewer)
+    (require 'eaf-pdf-viewer)
+    (require 'eaf-music-player)
+    (require 'eaf-org-previewer)
+    (require 'eaf-markdown-previewer)
+    (require 'eaf-rss-reader)
+    (require 'eaf-git)
+    ;; (require 'eaf-system-monitor)
+    ;; (require 'eaf-js-video-player)
     ;; (require 'eaf-map)
-    ;; (require 'eaf-rss-reader)
-    ;; (require 'eaf-mindmap)
     ;; (require 'eaf-markmap)
-    ;; (require 'eaf-markdown-previewer)
-    ;; (require 'eaf-org-previewer)
-    ;; (require 'eaf-file-manager) 
-    ;; (require 'eaf-file-browser)
-    ;; (require 'eaf-org)
+    ;; (require 'eaf-jupyter)
+    ;; (require 'eaf-2048)
+    ;; (require 'eaf-netease-cloud-music)
+    ;; (require 'eaf-mindmap)
     ;;-------------------------------------
+    (require 'eaf-org)
     ;; eaf-terminal
     (setq eaf-terminal-font-family "FiraCode Nerd Font"
-          eaf-terminal-font-size 16)
+          eaf-terminal-font-size 14)
     ;; TODO: hide mode-line
     ;; (advice-add 'eaf-open-terminal :filter-return #'spacemacs/toggle-mode-line-off)
     ;; (advice-add 'eaf-browser-open :filter-return #'spacemacs/toggle-mode-line-off)
@@ -107,9 +124,11 @@
     (add-to-list 'eaf-terminal-keybinding '("M-l" . "eaf-send-key-sequence"))
     ;; (add-to-list 'eaf-terminal-keybinding '("M-+" . "eaf-send-key-sequence"))
     ;; (add-to-list 'eaf-terminal-keybinding '("M--" . "eaf-send-key-sequence"))
+    ;; broot
     (add-to-list 'eaf-terminal-keybinding '("M-<return>" . "eaf-send-key-sequence"))
+    (add-to-list 'eaf-terminal-keybinding '("C-<left>" . "eaf-send-key-sequence"))
+    (add-to-list 'eaf-terminal-keybinding '("C-<right>" . "eaf-send-key-sequence"))
     ;;---------------------------------------
-
     ;; eaf-browser
     (setq eaf-browser-default-search-engine "google" ;; 设定eaf默认搜索引擎
           eaf-browser-enable-adblocker t   ;; 设定eaf开启广告屏蔽器
@@ -127,4 +146,18 @@
 
     (xy/set-eaf-browser-as-default)
     ;; (xy/set-google-chrome-as-default)
+
+    ;;------------------------------------------
+    ;; eaf-camera
+    (add-to-list 'eaf-camera-keybinding
+                 '("x" . "eaf-py-proxy-insert_or_close_buffer"))
+
+    ;;-------------------------------------------
+    ;; eaf-file-manager
+    (add-to-list 'eaf-file-manager-keybinding
+                 '("C-q" . "eaf-file-sender-qrcode"))
+
+    ;;-------------------------------------------
+    ;; eaf-file-browser
+
     ))
