c7c88c3a62d313ece8f1176110dcd96000d003ef
Xin <etimecowboy@gmail.com>
2023-06-14 10:07:17 +0800

    Add screenshot. Change some key bindings.

--- emacs-demo/packages.el
+++ emacs-demo/packages.el
@@ -1,6 +1,6 @@
 ;; -*- mode: emacs-lisp; lexical-binding: t -*-
 ;;; packages.el --- emacs-demo layer packages file for Spacemacs.
-;; Time-stamp: <2022-09-25 Sun 01:32 by xin on tufg>
+;; Time-stamp: <2023-06-14 Wed 02:06 by xin on tufg>
 ;; Author: etimecowboy <etimecowboy@gmail.com>
 ;;
 ;; This file is not part of GNU Emacs.
@@ -16,35 +16,36 @@
         command-log-mode
         gif-screencast
         keycast
+        (screenshot :location
+                    (recipe :fetcher github
+                            :repo "tecosaur/screenshot"))
         ))
 
 ;; load command-log-mode
 (defun emacs-demo/init-command-log-mode ()
-  (use-package command-log-mode
-    :init
-    (spacemacs/declare-prefix "aD" "demo")
-    (spacemacs/set-leader-keys "aDc" 'command-log-mode)
-    (spacemacs/set-leader-keys "aDl" 'clm/toggle-command-log-buffer)
-    (spacemacs/set-leader-keys "aDC" 'clm/command-log-clear)
-    (spacemacs/set-leader-keys "aDS" 'clm/command-log-save)))
+  (use-package command-log-mode))
 
 ;; load gif-screencast
 (defun emacs-demo/init-gif-screencast ()
   (use-package gif-screencast
-    :init
-    (spacemacs/declare-prefix "aD" "demo")
-    (spacemacs/set-leader-keys "aDg" 'gif-screencast)
     :bind
     (:map gif-screencast-mode-map
+          ("<f7>" . gif-screencast-start-or-stop)
           ("<f8>" . gif-screencast-toggle-pause)
           ("<f9>" . gif-screencast-stop))))
 
 ;; load keycast
 (defun emacs-demo/init-keycast ()
   (use-package keycast
-    :init
-    (spacemacs/declare-prefix "aD" "demo")
-    (spacemacs/set-leader-keys "aDt" 'keycast-tab-bar-mode)
     :config
     (setq keycast-tab-bar-location  'replace
           keycast-tab-bar-format "%k%2s%c%R")))
+
+;; load screenshot
+(defun emacs-demo/init-screenshot ()
+  (use-package screenshot
+    ;; :config
+    ;; (setq screenshot-border-width 0
+    ;;       screenshot-line-numbers-p t
+    ;;       screenshot-relative-line-numbers-p t)
+    ))
