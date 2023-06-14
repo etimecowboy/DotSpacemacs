f590ba395dec7ccd11d31189190f04b2333852eb
Xin <etimecowboy@gmail.com>
2023-06-14 10:08:14 +0800

    Change some keys.

--- compleseus-extra/keybindings.el
+++ compleseus-extra/keybindings.el
@@ -1,6 +1,6 @@
 ;; -*- mode: emacs-lisp; lexical-binding: t -*-
 ;;; keybindings.el --- compleseus-extra Layer keybindings File for Spacemacs
-;; Time-stamp: <2023-06-02 Fri 02:13 by xin on tufg>
+;; Time-stamp: <2023-06-08 Thu 06:37 by xin on tufg>
 ;; Author: etimecowboy <etimecowboy@gmail.com>
 ;;
 ;; This file is not part of GNU Emacs.
@@ -12,8 +12,8 @@
 ;;; Code:
 
 (global-set-key (kbd "M-\\") 'completion-at-point)
-(global-set-key (kbd "M-p") 'embark-act)
-(global-set-key (kbd "M-P") 'embark-act-noquit)
+(global-set-key (kbd "C-'") 'embark-act)
+(global-set-key (kbd "C-\"") 'embark-act-noquit)
 (global-set-key (kbd "M-L") 'embark-live)
 (global-set-key (kbd "M-B") 'embark-become)
 
