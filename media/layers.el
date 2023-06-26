4bd8ff7e2e10bb6e5a7f42b4934a10f41ea2b5fb
Xin <etimecowboy@gmail.com>
2023-06-26 22:03:54 +0800

    Minor.

---
+++ media/layers.el
@@ -0,0 +1,13 @@
+;;; layers.el --- Media layers File for Spacemacs
+;; Time-stamp: <2023-06-22 Thu 01:36 by xin on tufg>
+;; Author: etimecowboy <etimecowboy@gmail.com>
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+;;
+;;; Commentary:
+;;
+;;; Code:
+
+(configuration-layer/declare-layer-dependencies '(org))
