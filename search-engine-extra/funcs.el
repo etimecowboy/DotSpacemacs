ab271cf5d64a223d874b5d137a7af467cfb577fd
Xin <etimecowboy@gmail.com>
2023-07-05 09:24:31 +0800

    [search-engine-extra] change keybindings.

---
+++ search-engine-extra/funcs.el
@@ -0,0 +1,5 @@
+;; (defun xy/search-engine-select ()
+;;   "Set search engine to use with completing-read."
+;;   (interactive)
+;;   (call-interactively
+;;    (intern (completing-read "Chose Search Engine: " search-engine-alist))))
--- search-engine-extra/keybindings.el
+++ search-engine-extra/keybindings.el
@@ -1,6 +1,6 @@
 ;; -*- mode: emacs-lisp; lexical-binding: t -*-
 ;;; keybindings.el --- engine-extra Layer keybindings File for Spacemacs
-;; Time-stamp: <2022-11-23 Wed 06:57 by xin on tufg>
+;; Time-stamp: <2023-06-30 Fri 08:13 by xin on tufg>
 ;; Author: etimecowboy <etimecowboy@gmail.com>
 ;;
 ;; This file is not part of GNU Emacs.
@@ -11,12 +11,27 @@
 ;;
 ;;; Code:
 
+;; (spacemacs/declare-prefix "aw" "web search")
+
 (spacemacs/set-leader-keys
+  "aw/" nil ;; I use neither helm nor ivy.
+  "awa" 'engine/search-amazon
+  "awb" 'engine/search-bing
+  "awd" 'engine/search-docker-hub
+  "awD" 'engine/search-duck-duck-go
+  "awh" 'engine/search-github
   "awg" 'engine/search-google
   "awi" 'engine/search-google-images
-  "awG" 'engine/search-github
-  "awb" 'engine/search-bing
-  "aww" 'engine/search-wikipedia
-  "awe" 'engine/search-melpa
+  "awm" 'engine/search-google-maps
+  "awt" 'engine/search-twitter
+  "awG" 'engine/search-project-gutenberg
+  "awy" 'engine/search-youtube
   "aws" 'engine/search-stack-overflow
-  "awy" 'engine/search-youtube)
+  "aww" 'engine/search-wikipedia
+  "awp" 'engine/search-pip
+  "awP" 'engine/search-python-doc
+  "awc" 'engine/search-c++-api-reference
+  "awu" 'engine/search-ubuntu-packages
+  "awe" 'engine/search-melpa
+  "awC" 'engine/search-ctan
+  )
