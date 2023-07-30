cdfecb48d73bd04c0fae17f59594110ae9b03f8c
Xin <etimecowboy@gmail.com>
2023-07-30 11:09:00 +0800

    [lsp-bridge] add hydra menu.

--- lsp-bridge/keybindings.el
+++ lsp-bridge/keybindings.el
@@ -1,6 +1,6 @@
 ;; -*- mode: emacs-lisp; lexical-binding: t -*-
 ;;; keybindings.el --- lsp-bridge Layer keybindings File for Spacemacs
-;; Time-stamp: <2023-06-11 Sun 12:29 by xin on tufg>
+;; Time-stamp: <2023-07-29 Sat 13:24 by xin on tufg>
 ;; Author: etimecowboy <etimecowboy@gmail.com>
 ;;
 ;; This file is not part of GNU Emacs.
@@ -12,7 +12,6 @@
 ;;; Code:
 
 (spacemacs/set-leader-keys
-  "te" 'lsp-bridge-toggle-sdcv-helper
   "tq" 'lsp-bridge-mode
-  ;; "tr" 'xy/toggle-company-lsp-bridge
+  "L"  'spacemacs/lsp-bridge-transient-state/body
   )
--- lsp-bridge/packages.el
+++ lsp-bridge/packages.el
@@ -1,5 +1,5 @@
 ;;; packages.el --- lsp-bridge Layer packages File for Spacemacs
-;; Time-stamp: <2023-06-12 Mon 01:56 by xin on tufg>
+;; Time-stamp: <2023-07-30 Sun 02:36 by xin on tufg>
 ;; Author: etimecowboy <etimecowboy@gmail.com>
 ;;
 ;; This file is not part of GNU Emacs.
@@ -31,8 +31,7 @@
 
 (defun lsp-bridge/init-lsp-bridge ()
   (use-package lsp-bridge
-    ;; :commands
-    ;; (lsp-bridge-mode lsp-bridge-restart-process)
+    ;; NOTE: manually start lsp-bridge is better
     ;; :hook
     ;; (sh-mode . lsp-bridge-mode)
     ;; ;; (bash-ts-mode . lsp-bridge-mode)
@@ -51,36 +50,73 @@
     ;; (org-mode . lsp-bridge-mode)
     ;; (latex-mode . lsp-bridge-mode)
     ;; ;; (latex-ts-mode . lsp-bridge-mode)
+
+    :init
+    (spacemacs|define-transient-state lsp-bridge
+      :title "lsp-bridge transient state"
+      :doc "
+^Act^                     ^Find^                                 ^Doc^
+^^^^^^^^----------------------------------------------------------------------------------
+[_SPC_] complete menu     [_d_/_D_] definition/other window      [_?_] documentation
+[_E_] toggle sdcv helper  [_i_/_I_] implementation/other window  [_C-v_/_j_] scroll up
+[_P_] peek                [_t_/_T_] type/other window            [_M-v_/_k_] scroll down
+[_R_] restart             [_r_] find reference
+[_q_] quit
+"
+      :bindings
+      ("q" nil :exit t)
+      ("SPC" lsp-bridge-popup-complete-menu)
+      ("E" lsp-bridge-toggle-sdcv-helper)
+      ("R" lsp-bridge-restart-process)
+      ("p" lsp-bridge-peek)
+      ("P" lsp-bridge-peek-jump)
+      ("B" lsp-bridge-peek-jump-back)
+      ("T" lsp-bridge-peek-through)
+      ("F" lsp-bridge-code-format)
+      ("?" lsp-bridge-popup-documentation)
+      ("C-v" lsp-bridge-popup-documentation-scroll-up)
+      ("j" lsp-bridge-popup-documentation-scroll-up)
+      ("M-v" lsp-bridge-popup-documentation-scroll-down)
+      ("k" lsp-bridge-popup-documentation-scroll-down)
+      ("d" lsp-bridge-find-def)
+      ("D" lsp-bridge-find-def-other-window)
+      ("i" lsp-bridge-find-impl)
+      ("I" lsp-bridge-find-impl-other-window)
+      ("t" lsp-bridge-find-type-def)
+      ("T" lsp-bridge-find-type-def-other-window)
+      ("r" lsp-bridge-find-references)
+      )
+
+    :custom
+    ((lsp-bridge-python-lsp-server "pyright_ruff")
+     (lsp-bridge-c-lsp-server "ccls")
+     (lsp-bridge-tex-lsp-server "texlab")
+     ;; NOTE: To enable lsp-bridge in org-babel source blocks,
+     ;; You have to add the major mode to `org-src-lang-modes'
+     ;; REF: https://emacs-china.org/t/lsp-bridge/20786/3130
+     (lsp-bridge-enable-org-babel t)
+     (lsp-bridge-enable-completion-in-string t)
+     ;; (lsp-bridge-use-ds-pinyin-in-org-mode t)
+     ;; (lsp-bridge-enable-completion-in-minibuffer t)
+     (lsp-bridge-enable-hover-diagnostic t)
+     (acm-enable-quick-access t)
+     )
+
     :config
     (setq lsp-bridge-dir (file-name-directory (locate-library "lsp-bridge")))
     (add-to-list 'load-path (concat lsp-bridge-dir "core/"))
     (add-to-list 'load-path (concat lsp-bridge-dir "acm/"))
-    (setq lsp-bridge-c-lsp-server "ccls"
-          lsp-bridge-python-lsp-server "pyright_ruff"
-          lsp-bridge-tex-lsp-server "texlab"
-          lsp-bridge-use-ds-pinyin-in-org-mode nil
-          ;; lsp-bridge-enable-completion-in-minibuffer t
-          lsp-bridge-enable-hover-diagnostic t
-          ;; lsp-bridge-enable-completion-in-string t
-          lsp-bridge-enable-org-babel t
-          ;; NOTE: To enable lsp-bridge in org-babel source blocks,
-          ;; You have to add the major mode as the `org-src-lang-modes' string
-          ;; REF: https://emacs-china.org/t/lsp-bridge/20786/3130
-          )
-
-    (setq acm-enable-quick-access t
-          acm-quick-access-modifier 'control
+    (setq acm-quick-access-modifier 'control
           acm-backend-search-file-words-max-number 15)
 
-    ;; (unless (display-graphic-p)
-    ;;   (with-eval-after-load 'acm
+    (unless (display-graphic-p)
+      (with-eval-after-load 'acm
+        (require 'acm-terminal)))
+    ;; (with-eval-after-load 'acm
+    ;;   (unless (display-graphic-p)
     ;;     (require 'acm-terminal)))
 
-    (with-eval-after-load 'acm
-      (unless (display-graphic-p)
-        (require 'acm-terminal)))
-
-    ;; (global-lsp-bridge-mode) ;; lsp server may missing, too aggressive
+    ;; (global-lsp-bridge-mode) ;; NOTE: lsp server may missing
     ))
 
 (defun lsp-bridge/init-popon ()
