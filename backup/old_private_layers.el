e9df3f1352d06a541ea247a9b3341f62454765be
Xin <etimecowboy@gmail.com>
2023-08-24 09:49:28 +0800

    Add backups.

---
+++ backup/old_private_layers.el
@@ -0,0 +1,26 @@
+;;subed ;; merged into media layer
+;;emms ;; merged into media layer
+;;popweb ;; too heavy
+;;tabnine ;; remove all company stuff
+;;tree-sitter-extra ;; use emacs29 native treesit instead
+;;ui-tweak
+;;xwidgets
+;;hardhat
+;;english
+;;doom
+;;hyperbole ;; merged to compleseus-extra
+;;everywhere ;; merged into ui
+;;themes ;; merged into ui
+;; (python
+;;  :variables
+;;  python-backend 'anaconda
+;;  python-test-runner 'pytest
+;;  python-formatter 'black
+;;  python-save-before-test t
+;;  ;; python-fill-colum 85
+;;  )
+;;jupyter
+;; (conda
+;;  :variables
+;;  conda-anaconda-home "/opt/miniconda3/"
+;;  conda-env-home-directory "~/.conda/")
