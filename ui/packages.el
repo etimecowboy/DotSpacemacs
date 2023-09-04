524ddfc49088050a04134ea8fceda98ebf4ed942
Xin <etimecowboy@gmail.com>
2023-09-04 11:43:23 +0800

    [ui] add iscroll and god-mode packages.

--- ui/packages.el
+++ ui/packages.el
@@ -1,5 +1,5 @@
 ;;; packages.el --- UI layer packages File for Spacemacs
-;; Time-stamp: <2023-08-16 Wed 01:59 by xin on tufg>
+;; Time-stamp: <2023-09-01 Fri 09:01 by xin on tufg>
 ;; Author: etimecowboy <etimecowboy@gmail.com>
 ;;
 ;; This file is not part of GNU Emacs.
@@ -21,6 +21,8 @@
     hybrid-mode ;; belongs to spacemacs-bootstrap layer
     which-key ;; belongs to spacemacs-bootstrap layer
     persistent-scratch ;; belongs to spacemacs-editing layer
+    iscroll
+    god-mode
     ;; color-theme-sanityinc-tomorrow
     ;; zenburn-theme
     ;; (dockwin :location (recipe :fetcher github :repo "pronobis/dockwin")) ;; too old
@@ -89,6 +91,58 @@
 
 (defun ui/post-init-persistent-scratch ()
   (spacemacs|diminish persistent-scratch-mode))
+
+(defun ui/init-iscroll ()
+  (use-package emacs-everywhere
+    :defer t
+    :hook ((dired-mode
+            image-mode
+            org-mode
+            markdown-mode
+            eww-mode
+            w3m-mode
+            doc-view-mode
+            pdf-view-mode
+            ) . iscroll-mode)
+    :init
+    (spacemacs|diminish iscroll-mode)
+    ))
+
+(defun ui/init-god-mode ()
+  (use-package god-mode
+    :defer t
+    :bind (("<escape>" . god-mode-all)) ;; optional: switch <escape> and <capslock>
+    :init
+    (setq god-exempt-major-modes nil
+          god-exempt-predicates nil
+          god-mode-enable-function-key-translation nil
+          god-mode-alist '((nil . "C-")
+                           ("g" . "M-")
+                           ("G" . "C-M-")))
+    ;;(spacemacs|diminish god-mode " ✝" " God") ;; not 
+    :config
+    (when (featurep 'which-key)
+      (which-key-enable-god-mode-support))
+    ;;(spacemacs|diminish god-mode " ✝" " God")
+
+    (custom-set-faces '(god-mode-lighter
+                        ((t
+                          (:background "orange red"
+                                       :foreground "forest green"
+                                       :weight extra-bold)))))
+
+    (defun xy/toggle-cursor-display ()
+      (if (or god-local-mode buffer-read-only)
+          (progn
+            (setq cursor-type 'box)
+            (setq blink-cursor-interval 0.3)
+            (blink-cursor-mode 1))
+        (progn
+          (setq cursor-type 'bar)
+          (blink-cursor-mode -1))))
+
+    (add-hook 'post-command-hook #'xy/toggle-cursor-display)
+    ))
 
 ;; (defun ui/init-color-theme-sanityinc-tomorrow ()
 ;;   (use-package color-theme-sanityinc-tomorrow
