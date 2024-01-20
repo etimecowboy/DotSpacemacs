5bb28f40185f75f70c77fb4704460ea5b51edbc9
Xin <etimecowboy@gmail.com>
2024-01-20 11:28:14 +0800

    [browsers] - add a config variable ‘chinese-enable-avy-pinyin’ …
    that was defined in ‘chinese’ layer but required by some other layers.

---
+++ browsers/config.el
@@ -0,0 +1,4 @@
+;; FIXME: it seems `chinese-enable-avy-pinyin' is require by some package.
+;;
+;; let: Symbol’s value as variable is void: chinese-enable-avy-pinyin
+(setq chinese-enable-avy-pinyin t)
