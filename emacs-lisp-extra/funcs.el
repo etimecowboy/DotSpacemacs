2f47bece25ee4d995b4d752090a8fb9b209909f4
Xin <etimecowboy@gmail.com>
2024-03-06 16:16:35 +0800

    [emacs-lisp-extra] Add `asoc' package.

--- emacs-lisp-extra/funcs.el
+++ emacs-lisp-extra/funcs.el
@@ -1,5 +1,5 @@
 ;;; funcs.el --- emacs-lisp-extra Layer functions File for Spacemacs
-;; Time-stamp: <2023-05-08 Mon 10:56 by xin on tufg>
+;; Time-stamp: <2024-02-24 Sat 10:01 by xin on tufg>
 ;; Author: etimecowboy <etimecowboy@gmail.com>
 ;;
 ;; This file is not part of GNU Emacs.
@@ -20,3 +20,9 @@
 ;; example:
 ;; (add-list-to-list 'company-dabbrev-code-modes
 ;;                   '(c++-mode c-mode php-mode))
+
+;; REF: https://emacs.stackexchange.com/questions/38008/adding-many-items-to-a-list
+;; (defun merge-list-to-list (dst src)
+;;   "Merges content of the 2nd list with the 1st one"
+;;   (set dst
+;;        (append (eval dst) src)))
--- emacs-lisp-extra/packages.el
+++ emacs-lisp-extra/packages.el
@@ -1,6 +1,6 @@
 ;; -*- mode: emacs-lisp; lexical-binding: t -*-
 ;;; packages.el --- emacs-lisp-extra layer packages file for Spacemacs.
-;; Time-stamp: <2023-05-08 Mon 11:06 by xin on tufg>
+;; Time-stamp: <2024-02-24 Sat 10:07 by xin on tufg>
 ;; Author: etimecowboy <etimecowboy@gmail.com>
 ;;
 ;; This file is not part of GNU Emacs.
@@ -14,7 +14,14 @@
 (setq emacs-lisp-extra-packages
       '(
         ;; cask
+        (asoc :location (recipe
+                         :fetcher github
+                         :repo "troyp/asoc.el"))
         ))
 
 ;; (defun emacs-lisp-extra/init-cask ()
 ;;   (use-package cask))
+
+(defun emacs-lisp-extra/init-asoc ()
+  (use-package asoc
+    :ensure t))
