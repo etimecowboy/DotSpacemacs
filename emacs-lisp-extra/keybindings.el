2048247bbb1ae02d3856265190a190600a3a5d73
Xin <etimecowboy@gmail.com>
2024-03-13 11:20:12 +0800

    [emacs-lisp-extra] Add keys to save kmacro.

---
+++ emacs-lisp-extra/keybindings.el
@@ -0,0 +1,15 @@
+;; -*- mode: emacs-lisp; lexical-binding: t -*-
+;;; keybindings.el --- emacs-lisp-extra Layer keybindings File for Spacemacs
+;; Time-stamp: <2024-03-09 Sat 14:11 by xin on tufg>
+;; Author: etimecowboy <etimecowboy@gmail.com>
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+;;
+;;; Commentary:
+;;
+;;; Code:
+
+(global-set-key (kbd "C-x C-k a") 'xy/save-kmacro)
+(global-set-key (kbd "C-x C-k i") 'insert-kbd-macro)
