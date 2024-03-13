d10c31044743a46cd98666e2beadd494584264ab
Xin <etimecowboy@gmail.com>
2024-03-13 11:23:00 +0800

    [emacs-lisp-extra] Add global key for xy/insert-kbd-macro.

--- emacs-lisp-extra/keybindings.el
+++ emacs-lisp-extra/keybindings.el
@@ -1,6 +1,6 @@
 ;; -*- mode: emacs-lisp; lexical-binding: t -*-
 ;;; keybindings.el --- emacs-lisp-extra Layer keybindings File for Spacemacs
-;; Time-stamp: <2024-03-09 Sat 14:11 by xin on tufg>
+;; Time-stamp: <2024-03-13 Wed 03:22 by xin on tufg>
 ;; Author: etimecowboy <etimecowboy@gmail.com>
 ;;
 ;; This file is not part of GNU Emacs.
@@ -11,5 +11,5 @@
 ;;
 ;;; Code:
 
-(global-set-key (kbd "C-x C-k a") 'xy/save-kmacro)
-(global-set-key (kbd "C-x C-k i") 'insert-kbd-macro)
+(global-set-key (kbd "C-x C-k i") 'xy/insert-kbd-macro)
+;; (global-set-key (kbd "C-x C-k i") 'insert-kbd-macro)
