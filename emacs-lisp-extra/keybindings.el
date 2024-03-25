ccebb7e000a60609e2b073fee6c4e2fb2311d4d4
Xin <etimecowboy@gmail.com>
2024-03-25 17:48:23 +0800

    [emacs-extra] Add `esup' package.

--- emacs-lisp-extra/keybindings.el
+++ emacs-lisp-extra/keybindings.el
@@ -1,6 +1,6 @@
 ;; -*- mode: emacs-lisp; lexical-binding: t -*-
 ;;; keybindings.el --- emacs-lisp-extra Layer keybindings File for Spacemacs
-;; Time-stamp: <2024-03-13 Wed 03:22 by xin on tufg>
+;; Time-stamp: <2024-03-21 Thu 03:58 by xin on tufg>
 ;; Author: etimecowboy <etimecowboy@gmail.com>
 ;;
 ;; This file is not part of GNU Emacs.
@@ -10,6 +10,9 @@
 ;;; Commentary:
 ;;
 ;;; Code:
+
+(spacemacs/set-leader-keys
+  "hb" 'esup)
 
 (global-set-key (kbd "C-x C-k i") 'xy/insert-kbd-macro)
 ;; (global-set-key (kbd "C-x C-k i") 'insert-kbd-macro)
--- emacs-lisp-extra/packages.el
+++ emacs-lisp-extra/packages.el
@@ -1,6 +1,6 @@
 ;; -*- mode: emacs-lisp; lexical-binding: t -*-
 ;;; packages.el --- emacs-lisp-extra layer packages file for Spacemacs.
-;; Time-stamp: <2024-02-24 Sat 10:07 by xin on tufg>
+;; Time-stamp: <2024-03-21 Thu 03:57 by xin on tufg>
 ;; Author: etimecowboy <etimecowboy@gmail.com>
 ;;
 ;; This file is not part of GNU Emacs.
@@ -12,16 +12,26 @@
 ;;; Code:
 
 (setq emacs-lisp-extra-packages
-      '(
+      '((asoc :location (recipe :fetcher github :repo "troyp/asoc.el"))
+        esup ;; record bootup time
         ;; cask
-        (asoc :location (recipe
-                         :fetcher github
-                         :repo "troyp/asoc.el"))
         ))
-
-;; (defun emacs-lisp-extra/init-cask ()
-;;   (use-package cask))
 
 (defun emacs-lisp-extra/init-asoc ()
   (use-package asoc
     :ensure t))
+
+(defun emacs-lisp-extra/init-esup ()
+  (use-package esup
+    :commands esup
+    ;; To use MELPA Stable use ":pin melpa-stable",
+    ;; :pin melpa
+    :config
+    (setq esup-user-init-file "~/src/spacemacs/init.el"
+          esup-depth 3
+          esup-insignificant-time 0.001)
+    ))
+
+
+;; (defun emacs-lisp-extra/init-cask ()
+;;   (use-package cask))
