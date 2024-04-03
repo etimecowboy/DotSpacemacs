3aad604a92bd364e9d30cb7f19570947bdbe0bea
Xin <etimecowboy@gmail.com>
2024-04-03 15:44:31 +0800

    Backup treemacs-extra layer.

---
+++ backup/treemacs-extra/packages.el
@@ -0,0 +1,45 @@
+;;; packages.el --- treemacs-extra layer packages file for Spacemacs.
+;; Time-stamp: <2024-04-03 Wed 03:14 by xin on tufg>
+;; Author: etimecowboy <etimecowboy@gmail.com>
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+;;
+;;; Commentary:
+;;
+;;; Code:
+
+(defconst treemacs-extra-packages '(treemacs))
+
+(defun treemacs-extra/pre-init-treemacs ()
+  (spacemacs|use-package-add-hook treemacs
+    :post-init
+    ;; REF: https://github.com/Alexander-Miller/treemacs/issues/842
+    (add-hook 'treemacs-mode-hook #'aorst/treemacs-setup-title)
+
+    :post-config
+    (defun aorst/treemacs-setup-title ()
+      ;; dynamic workspace title
+      ;; REF: https://andreyor.st/posts/2020-05-01-dynamic-title-for-treemacs-workspace/
+      (let ((bg (face-attribute 'default :background))
+            (fg (face-attribute 'default :foreground)))
+        (face-remap-add-relative 'header-line
+                                 :background bg
+                                 :foreground fg
+                                 :box `(:line-width ,(/ (line-pixel-height) 5)
+                                        :color ,bg)))
+      (setq header-line-format
+            '((:eval
+               (let* ((text (treemacs-workspace->name (treemacs-current-workspace)))
+                      (extra-align (+ (/ (length text) 2) 1))
+                      (width (- (/ (window-width) 2) extra-align)))
+                 (concat (make-string width ?\s) text)))))
+
+      ;; smaller font size in graphic mode
+      (when window-system
+        (text-scale-decrease 1)))
+
+    (treemacs-define-RET-action 'file-node-closed #'treemacs-visit-node-ace)
+    (treemacs-define-RET-action 'file-node-open #'treemacs-visit-node-ace)
+    ))
