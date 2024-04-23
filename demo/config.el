e3733d43f71bc3c944a547000d416301c05d0b44
Xin <etimecowboy@gmail.com>
2024-04-23 10:16:53 +0800

    [demo] add layer config.

---
+++ demo/config.el
@@ -0,0 +1,18 @@
+;;; config.el --- demo configuration File for Spacemacs
+;; Time-stamp: <2024-04-18 Thu 16:56 by xin on tufg>
+;; Author: etimecowboy <etimecowboy@gmail.com>
+
+(defvar xy:fixed-pitch-font "Noto Mono CJK SC"
+  "The font to use for monospaced text. `fixed-pitch' face.")
+
+(defvar xy:fixed-pitch-serif-font "Noto Serif CJK SC"
+  "The serif font to use for monospaced text. `fixed-pitch-serif' face.")
+
+(defvar xy:variable-pitch-font "Noto Sans CJK SC"
+  "The font to use for proportional text. `variable-pitch' face.")
+
+(defvar xy:variable-pitch-text-height 1.1
+  "The height scale for `variable-pitch-text' face.")
+
+(defvar xy:display-mixed-pitch nil
+  "Activate / deactivate mixed-pitch-mode in specific modes.")
