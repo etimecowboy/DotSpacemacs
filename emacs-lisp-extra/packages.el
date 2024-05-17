b52e9dc9d6c1b2a37c4bb95c289d617fcafc12d0
Xin <etimecowboy@gmail.com>
2024-05-17 09:35:24 +0800

    [emacs-lisp-extra] remove eval-sexp-fu,
    because it is now a part of spacemacs-editing.

--- emacs-lisp-extra/packages.el
+++ emacs-lisp-extra/packages.el
@@ -1,6 +1,6 @@
 ;; -*- mode: emacs-lisp; lexical-binding: t -*-
 ;;; packages.el --- emacs-lisp-extra layer packages file for Spacemacs.
-;; Time-stamp: <2024-03-21 Thu 03:57 by xin on tufg>
+;; Time-stamp: <2024-05-06 Mon 07:14:07 GMT by xin on tufg>
 ;; Author: etimecowboy <etimecowboy@gmail.com>
 ;;
 ;; This file is not part of GNU Emacs.
@@ -14,6 +14,7 @@
 (setq emacs-lisp-extra-packages
       '((asoc :location (recipe :fetcher github :repo "troyp/asoc.el"))
         esup ;; record bootup time
+        ;; eval-sexp-fu
         ;; cask
         ))
 
@@ -32,6 +33,9 @@
           esup-insignificant-time 0.001)
     ))
 
+;; (defun emacs-lisp-extra/init-eval-sexp-fu ()
+;;   (use-package eval-sexp-fu
+;;     :ensure t))
 
 ;; (defun emacs-lisp-extra/init-cask ()
 ;;   (use-package cask))
