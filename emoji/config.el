b4b3bc93c47608bd07d4dd9e74f9f0c161bdddf9
Xin <etimecowboy@gmail.com>
2024-05-17 09:42:12 +0800

    [emoji] Add config.el.

---
+++ emoji/config.el
@@ -0,0 +1,13 @@
+;;; config.el --- Emoji configuration File for Spacemacs
+;; Time-stamp: <2024-05-17 Fri 01:06:39 GMT by xin on tufg>
+;; Author: etimecowboy <etimecowboy@gmail.com>
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+;;
+;;; Commentary:
+;;
+;;; Code:
+
+(xy/set-emoji-font)
