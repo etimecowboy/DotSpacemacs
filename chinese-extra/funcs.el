ee8f9b3d16eb5f63b925af0e4075f6acc7f5ae71
Xin <etimecowboy@gmail.com>
2024-05-17 09:36:48 +0800

    [chinese-extra] Change xy/set-font, add google-translate keys.

--- chinese-extra/funcs.el
+++ chinese-extra/funcs.el
@@ -1,5 +1,5 @@
 ;;; funcs.el --- Chinese-extra Layer functions File for Spacemacs
-;; Time-stamp: <2024-04-14 Sun 07:41 by xin on tufg>
+;; Time-stamp: <2024-04-25 Thu 07:45 by xin on tufg>
 ;; Author: etimecowboy <etimecowboy@gmail.com>
 ;;
 ;; This file is not part of GNU Emacs.
@@ -16,53 +16,53 @@
 ;;       1234567890i1l|!0oO{[()]}<>?/¬`'"%^+-~#@*:;\ --> ~~>
 ;;       一二三四五六七八九十
 ;;       12345678901234567890
-(defun font-installed-p (font-name)
-  "Check if font with FONT-NAME is available."
-  (if (find-font (font-spec :name font-name))
-      t
-    nil))
+;; (defun font-installed-p (font-name)
+;;   "Check if font with FONT-NAME is available."
+;;   (if (find-font (font-spec :name font-name))
+;;       t
+;;     nil))
 
-(defun xy/set-fonts ()
-  "Setup fonts for default, Chinese, and emoji."
-  (interactive)
-  (when (display-graphic-p)
+;; (defun xy/set-fonts ()
+;;   "Setup fonts for default, Chinese, and emoji."
+;;   (interactive)
+;;   (when (display-graphic-p)
 
-    ;; Set default font
-    (cl-loop for font in
-             '("Cascadia Code"
-               "Fira Code"
-               "Iosevka"
-               "Source Code Pro"
-               "Consolas"
-               "Jetbrains Mono"
-               "Hack"
-               "Menlo"
-               "DejaVu Sans Mono")
-             when (font-installed-p font)
-             return (set-face-attribute 'default nil :family font :height 110))  ;; 130
+;;     ;; Set default font
+;;     (cl-loop for font in
+;;              '("Cascadia Code"
+;;                "Fira Code"
+;;                "Iosevka"
+;;                "Source Code Pro"
+;;                "Consolas"
+;;                "Jetbrains Mono"
+;;                "Hack"
+;;                "Menlo"
+;;                "DejaVu Sans Mono")
+;;              when (font-installed-p font)
+;;              return (set-face-attribute 'default nil :family font :height 110))  ;; 130
 
-    ;; Set font for Chinese characters
-    (cl-loop for font in
-             '("FZDaHei-B02"
-               "Adobe Fangsong Std"
-               "WenQuanYi Micro Hei Mono"
-               ;; "FZDaHei-B02"
-               "Microsoft Yahei"
-               "Adobe Heiti Std"
-               "FZXiaoBiaoSong-B05"
-               "Adobe Song Std"
-               "Adobe Kaiti Std"
-               "LXGW WenKai Mono GB")
-             when (font-installed-p font)
-             return (progn
-                      (setq face-font-rescale-alist `((,font . 1.25)))  ;; 1.2
-                      (set-fontset-font t 'han (font-spec :family font))))))
+;;     ;; Set font for Chinese characters
+;;     (cl-loop for font in
+;;              '("FZDaHei-B02"
+;;                "Adobe Fangsong Std"
+;;                "WenQuanYi Micro Hei Mono"
+;;                ;; "FZDaHei-B02"
+;;                "Microsoft Yahei"
+;;                "Adobe Heiti Std"
+;;                "FZXiaoBiaoSong-B05"
+;;                "Adobe Song Std"
+;;                "Adobe Kaiti Std"
+;;                "LXGW WenKai Mono GB")
+;;              when (font-installed-p font)
+;;              return (progn
+;;                       (setq face-font-rescale-alist `((,font . 1.25)))  ;; 1.2
+;;                       (set-fontset-font t 'han (font-spec :family font))))))
 
 ;; Set chinese font
 ;;
-;; Based on "~/src/spacemacs/layers/+intl/chinese/config.el#set-monospaced-font"
-;;
 ;; -------------------------------------------------------------------------------------
+;;
+;; Based on "~/src/spacemacs/layers/+intl/chinese/config.el#set-monospaced-font"
 ;;
 ;; ;; Set the monospaced font size when mixed Chinese and English words
 ;; (defun spacemacs//set-monospaced-font (english chinese english-size chinese-size)
@@ -96,18 +96,20 @@
 ;; -----------------------------------------------------------------------------------------
 ;;
 ;; Set the monospaced font size when mixed Chinese and English words
-(defun spacemacs//set-monospaced-font (english chinese english-size chinese-size)
-  (interactive)
-  (set-face-attribute 'default nil :font
-                      (format   "%s:pixelsize=%d"  english english-size))
-  (set-face-attribute 'fixed-pitch nil :font
-                      (format   "%s:pixelsize=%d"  english english-size))
-  (set-face-attribute 'variable-pitch nil :font
-                      (format   "%s:pixelsize=%d"  "DejaVu Sans Condensed" english-size))
-  (dolist (charset '(kana han cjk-misc bopomofo))
-    (set-fontset-font (frame-parameter nil 'font) charset
-                      (format "-*-%s-*-*-*-*-%d-*-*-*-*-*-*-*" chinese chinese-size)))
-  (redraw-display))
+;; (defun spacemacs//set-monospaced-font (english chinese english-size chinese-size)
+;;   (interactive)
+;;   (set-face-attribute 'default nil :font
+;;                       (format   "%s:pixelsize=%d"  english english-size))
+;;   (set-face-attribute 'fixed-pitch nil :font
+;;                       (format   "%s:pixelsize=%d"  english english-size))
+;;   (set-face-attribute 'variable-pitch nil :font
+;;                       (format   "%s:pixelsize=%d"  "DejaVu Sans Condensed" english-size))
+;;   (dolist (charset '(kana han cjk-misc bopomofo))
+;;     (set-fontset-font (frame-parameter nil 'font) charset
+;;                       (format "-*-%s-*-*-*-*-%d-*-*-*-*-*-*-*" chinese chinese-size)))
+;;   (redraw-display))
+;;
+;; NOTE: Replaced by functions `xy/set-face-font' and `xy/set-cjk-font' of my ui layer.
 
 ;; 从剪贴板获取内容
 (defun clipboard/get ()
