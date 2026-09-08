49fc787f53b816c9ca2c3b6af735226e5a15f1c2
Xin <etimecowboy@gmail.com>
2026-09-09 05:46:35 +0800

    Continue with last commit ...
    1. Add patch file scripts/image.c.patch
    2. Add applying the patch in update_emacs_code.sh.

--- scripts/apply_patch.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd ~/src/emacs
-git apply xin/image.c.patch
--- scripts/update_emacs_code.sh
+++ scripts/update_emacs_code.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 cd ~/src/emacs
+git checkout src/image.c
 git fetch --all
 git pull
+git apply ~/src/DotSpacemacs/scripts/image.c.patch
